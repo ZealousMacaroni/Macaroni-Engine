3facc54b149020a09e35b4a80241a827d8912226
William Mix <willimapmix@gmail.com>
2024-06-27 18:01:30 -0500

    Created Hello Triangle example code for the engine.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main() { 
     glfwInit();
     
-    Window win(720, 720, "app", 10);
+    Window win(640, 480, "app", 1);
 
     win.modifyWindowAttribute(GLFW_CONTEXT_VERSION_MAJOR, 4);
     win.modifyWindowAttribute(GLFW_CONTEXT_VERSION_MINOR, 1);
@@ -25,15 +25,13 @@ int main() {
     shader.useProgram();
     
     float points[32] = {
-        //positions           //color coords      //tex coords
-        -0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-        0.5f, -0.5f, 0.0f,    0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
-        0.5f, 0.5f, 0.0f,     0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-        -0.5f, 0.5f, 0.0f,    0.5f, 0.5f, 0.0f,   0.0f, 1.0f
+        -0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, 0.0f,    0.0f, 1.0f, 0.0f,
+        0.0f, 0.5f, 0.0f,     0.0f, 0.0f, 1.0f,
     };
 
-    unsigned int indices[6] = {
-        0, 1, 2, 2, 3, 0
+    unsigned int indices[3] = {
+        0, 1, 2
     };
 
     unsigned int VAO;
@@ -50,28 +48,11 @@ int main() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer_indices);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, 0);
-    glEnableVertexAttribArray(0); //this binds the vertex attrib pointer to the currently bound array buffer at index 0 of the VAO
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+    glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float) * 3));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (void*)(sizeof(float) * 3));
     glEnableVertexAttribArray(1);
-
-    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float) * 6));
-    glEnableVertexAttribArray(2);
-
-    Texture tex0(GL_TEXTURE_2D, 0, GL_RGB, GL_RGBA, true);
-
-    tex0.loadData("container.jpeg", true);
-    tex0.createTexture();
-    tex0.loadDataIntoTexture();
-    tex0.setSamplerUniform(shader.getProgramID());
-
-    Matrix4x4 scaleMatrix;
-    scaleMatrix.setIdentity(2.0f);
-    float* matrix =scaleMatrix.get();
-
-    unsigned int transformLocation = glGetUniformLocation(shader.getProgramID(), "transform");
-    glUniformMatrix4fv(transformLocation, 1, GL_TRUE, scaleMatrix.get());
     
     while (!glfwWindowShouldClose(win.getWindow())) {
         glClear(GL_COLOR_BUFFER_BIT);
--- shaders/fragmentShader.frag
+++ shaders/fragmentShader.frag
@@ -1,12 +1,9 @@
 #version 410 core
 
 in vec3 vertColor;
-in vec2 vertTexCoords;
-
-uniform sampler2D u_texImg0;
 
 out vec4 fragmentColor;
 
 void main() {
-    fragmentColor = texture(u_texImg0, vertTexCoords) * vec4(vertColor, 0.0);
+    fragmentColor = vec4(vertColor, 0.0);
 }
