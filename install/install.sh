58740b5361bfcf205854fcfdb2ebe4cb506ad876
ZealousMacaroni <138621312+ZealousMacaroni@users.noreply.github.com>
2024-08-18 13:57:26 -0500

    Delete install directory
    Complete revamp of the engine stemming from deleting all current files and uploading new files.

--- install/install.sh
+++
@@ -1,6 +0,0 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
-brew install glew
-brew install glfw
-brew install jsoncpp
